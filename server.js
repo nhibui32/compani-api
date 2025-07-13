99fc6d2d26d38edbc7382ba39697c01c033779df
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2025-07-12 23:16:09 -0500

    :first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+
+// Routes
+app.use('/api/departments', require('./routes/departments'));
+// app.use('/api/employees', require('./routes/employees'));
+// app.use('/api/salaries', require('./routes/salaries'));
+// app.use('/api/products', require('./routes/products'));
+// app.use('/api/sales', require('./routes/sales'));
+// app.use('/api/stock', require('./routes/stock'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
