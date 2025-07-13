875e01522d24f0b0b82bc050892f20c90a5ac6ae
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2025-07-13 18:40:15 -0500

    update get employees by department id. use http://localhost:3000/api/employees/department/3 to test

--- routes/employees.js
+++ routes/employees.js
@@ -18,9 +18,23 @@ router.get('/:idEmployees', async(req, res)=>{
     try{
         const [rows] = await db.query('SELECT * FROM Employees WHERE idEmployees = ?', [idEmployees]);
         if (rows.length === 0 ){
-            return res.status(400).json({error: 'Employees with this id not found'})
+            return res.status(404).json({error: 'Employees with this id not found'})
         }
         res.json(rows[0])
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+})
+
+// get employees by department 
+router.get('/department/:idDepartment', async (req, res) =>{
+    const {idDepartment} = req.params;
+    try{
+        const[rows] = await db.query(' SELECT * FROM Employees WHERE idDepartment = ?', [idDepartment]);
+        if (rows.length === 0){
+            return res.status(404).json({error: 'Employees in this Department not found'})
+        }
+        res.json(rows)
     }catch(err){
         res.status(500).json({error: err.message})
     }
