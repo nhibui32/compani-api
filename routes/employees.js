01a80797b55b06468af75a5bd0b9d86fed1bfbee
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2025-07-13 20:51:49 -0500

    update put and delete method for employees.js

--- routes/employees.js
+++ routes/employees.js
@@ -40,4 +40,62 @@ router.get('/department/:idDepartment', async (req, res) =>{
     }
 })
 
+// post a new employees 
+router.post('/', async(req, res)=>{
+    const {idEmployees, name, email, idDepartment, hireDate} = req.body;
+    if (!idEmployees || !name || !email || !idDepartment || !hireDate) {
+        return res.status(400).json({error: 'id employee, name, email, id department and hire date are required'});
+    }
+
+    try{
+        const [result] = await db.query(
+            'INSERT INTO Employees (idEmployees, name, email, idDepartment, hireDate) VALUES ( ?, ?, ?, ?, ?)', [idEmployees, name, email, idDepartment, hireDate]
+        );
+        res.status(201).json({idEmployees, name, email, idDepartment, hireDate})
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+})
+
+// put (update) an employees
+router.put('/:idEmployees', async(req, res)=>{
+    const{idEmployees} = req.params;
+    const{name, email, idDepartment, hireDate} = req.body;
+    if( !name || !email || !idDepartment || !hireDate){
+        return res.status(400).json({error: "name, email, id department, hire date are required"})
+    };
+
+    try{
+        const [result] = await db.query(
+            'UPDATE Employees SET name = ?, email =?, idDepartment = ?, hireDate = ? WHERE idEmployees = ?', [name, email, idDepartment, hireDate, idEmployees]
+        );
+        if(result.affectedRows === 0){
+            return res.status(404).json({error: 'Employees not found'});
+        }
+        res.json({message: 'Employee updated successfully'})
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+})
+
+
+// delete an employee 
+router.delete('/:idEmployees', async(req, res)=>{
+    const {idEmployees} = req.params;
+
+    try{
+        // Step 1: Delete related salary records
+        await db.query('DELETE FROM Salaries WHERE idEmployees = ?', [idEmployees]);
+
+        // Step 2: Delete the employee
+        const [results] = await db.query('DELETE FROM Employees WHERE idEmployees = ?', [idEmployees]);
+
+        if(results.affectedRows === 0){
+            return res.status(404).json({error: 'Employee not found'});
+        }
+        res.json({message: 'Employee deleted successfully'});
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+});
 module.exports = router
