e13e8a748880c85da8997eab89a775ab0dade5ba
Ngoc Anh Nhi Bui <148082202+nhibui32@users.noreply.github.com>
2025-07-19 11:37:38 -0500

    use http://localhost:3000/api/salaries?idEmployees=1 to get all salaries for an employee

--- routes/salaries.js
+++ routes/salaries.js
@@ -2,15 +2,7 @@ const express = require('express')
 const router = express.Router();
 const db = require('../db');
 
-// get all the salaries record 
-router.get('/', async(req, res)=>{
-    try{
-        const [rows] = await db.query('SELECT * FROM Salaries');
-        res.json(rows)
-    }catch(err){
-        res.status(500).json({error: err.message});
-    }
-})
+
 
 // get salary by id 
 router.get('/:idSalaries', async(req,res)=>{
@@ -26,19 +18,24 @@ router.get('/:idSalaries', async(req,res)=>{
     }
 })
 
-// // // get all salaries for an employees 
-// router.get('/:idEmployees', async(req, res)=>{
-//     const {idEmployees} = req.params
-//     try{
-//         const[rows] = await db.query('SELECT * FROM Salaries WHERE idEmployees = ?', [idEmployees]);
-//         if(rows.length === 0){
-//             return res.status(404).json({error: 'Employees not found'});
-//         }
-//         res.json(rows)
-//     }catch(err){
-//         res.status(500).json({error: err.message})
-//     }
-// })
+// get all salaries for an employee 
+router.get('/', async(req, res)=>{
+    const{idEmployees} = req.query;
+    try{
+        let query = 'SELECT * FROM Salaries ';
+        let params = [];
+
+        if(idEmployees){
+            query += 'WHERE idEmployees = ?';
+            params.push(idEmployees);
+        }
+
+        const [rows] = await db.query(query , params);
+        res.json(rows);
+    }catch(err){
+        res.status(500).json({error: err.message})
+    }
+})
 
 
 module.exports = router
